0d1300477a296a51ed852654c98153759722ca90
Kohei Tabata <nerd0geek1@gmail.com>
2016-06-12 01:25:41 +0900

    define TimerType as Timer interface

---
+++ Timer/Timer/classes/TimerType.swift
@@ -0,0 +1,28 @@
+//
+//  TimerType.swift
+//  Timer
+//
+//  Created by Kohei Tabata on 6/11/16.
+//  Copyright © 2016 Kohei Tabata. All rights reserved.
+//
+
+import Foundation
+
+public protocol TimerType {
+    var didStart: (() -> Void)? { get set }
+    var didPause: (() -> Void)? { get set }
+    var didResume: (() -> Void)? { get set }
+    var didStop: ((isFinished: Bool) -> Void)? { get set }
+
+    func setDuration(duration: NSTimeInterval)
+
+    func start(startDate: NSDate)
+    func pause(pauseDate: NSDate)
+    func resume(resumeDate: NSDate)
+    func stop()
+
+    func isRunning(now: NSDate) -> Bool
+
+    func remainingDuration(now: NSDate) -> NSTimeInterval
+    func formattedRemainingDuration(now: NSDate) -> String
+}
