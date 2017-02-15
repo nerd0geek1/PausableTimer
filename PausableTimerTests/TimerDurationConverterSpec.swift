2551d83e6fe14a739442386a7a2630a84b97b024
Kohei Tabata <nerd0geek1@gmail.com>
2017-02-15 09:56:45 +0900

    improve Swift code

--- PausableTimerTests/TimerDurationConverterSpec.swift
+++ PausableTimerTests/TimerDurationConverterSpec.swift
@@ -13,19 +13,19 @@ import Nimble
 
 class TimerDurationConverterSpec: QuickSpec {
     override func spec() {
-        describe("durationString(duration: TimeInterval)", {
+        describe("durationString(from duration: TimeInterval)", {
             context("with duration which exceeds one hour", {
                 it("will return string like format 00:00:00", closure: {
                     let duration: TimeInterval = 3820
 
-                    expect(TimerDurationConverter.durationString(duration)).to(equal("01:03:40"))
+                    expect(TimerDurationConverter.durationString(from: duration)).to(equal("01:03:40"))
                 })
             })
             context("with duration which does not exceeds one hour", {
                 it("will return string like format 00:00", closure: {
                     let duration: TimeInterval = 1815
                     
-                    expect(TimerDurationConverter.durationString(duration)).to(equal("30:15"))
+                    expect(TimerDurationConverter.durationString(from: duration)).to(equal("30:15"))
                 })
             })
         })
