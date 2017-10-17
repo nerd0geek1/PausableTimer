c985af84d11c0819780744488d538feeb57b6b11
Kohei Tabata <nerd0geek1@gmail.com>
2017-10-17 11:48:16 +0900

    refactor PausableTimer and its test cases

--- PausableTimer/PausableTimer/classes/PausableTimer.swift
+++ PausableTimer/PausableTimer/classes/PausableTimer.swift
@@ -2,7 +2,9 @@ import Foundation
 
 public class PausableTimer: PausableTimerType {
 
-    public static let sharedInstance: PausableTimer = PausableTimer()
+    // MARK: - public properties
+
+    public static let shared: PausableTimer = PausableTimer()
 
     // MARK: - callback closures
 
@@ -11,16 +13,15 @@ public class PausableTimer: PausableTimerType {
     public var didResume: (() -> Void)?
     public var didStop: ((_ isFinished: Bool) -> Void)?
 
-    // MARK: - duration, and related variables
+    // MARK: - private properties
 
     private var timer: Timer?
-
-    private var startDate: Date?
 
     private var duration: TimeInterval        = 0
     private var currentDuration: TimeInterval = 0
 
-    // MARK: - setup
+    // MARK: - public methods
+    // MARK: setup
 
     public func set(duration: TimeInterval) {
         self.duration        = duration
@@ -30,34 +31,25 @@ public class PausableTimer: PausableTimerType {
     // MARK: - operate
 
     public func start(at startDate: Date = Date()) {
-        registerTimer()
+        registerTimer(with: startDate)
 
-        self.startDate = startDate
         didStart?()
     }
 
     public func pause(at pauseDate: Date = Date()) {
-        if !isRunning(at: pauseDate) {
-            return
-        }
+        if !isRunning(at: pauseDate) { return }
 
         self.currentDuration = remainingDuration(at: pauseDate)
-        self.startDate       = nil
         self.timer?.invalidate()
 
         didPause?()
     }
 
     public func resume(at resumeDate: Date = Date()) {
-        if isRunning(at: resumeDate) {
-            return
-        }
-        if remainingDuration(at: resumeDate) == 0 {
-            return
-        }
+        if isRunning(at: resumeDate) {              return }
+        if remainingDuration(at: resumeDate) == 0 { return }
 
-        registerTimer()
-        self.startDate = resumeDate
+        registerTimer(with: resumeDate)
 
         didResume?()
     }
@@ -68,49 +60,47 @@ public class PausableTimer: PausableTimerType {
         didStop?(false)
     }
 
-    // MARK: - Timer
-
-    @objc
-    private func didFinishTimerDuration() {
-        reset()
-
-        didStop?(true)
-    }
-
     // MARK: -
 
     public func isRunning(at now: Date = Date()) -> Bool {
-        if startDate == nil {
-            return false
-        }
+        guard let timer = timer, timer.isValid else { return false }
 
         return remainingDuration(at: now) > 0
     }
 
     public func remainingDuration(at now: Date = Date()) -> TimeInterval {
-        guard let startDate: Date = startDate else {
+        guard let timer = timer, timer.isValid else {
             return currentDuration
         }
 
-        let elapsedDuration: TimeInterval   = now.timeIntervalSince(startDate)
+        let elapsedDuration: TimeInterval   = now.timeIntervalSince(timer.fireDate)
         let remainingDuration: TimeInterval = currentDuration - elapsedDuration
 
         return remainingDuration < 0 ? 0 : remainingDuration
     }
 
+    // MARK: - Timer
+
+    @objc private func didFinishTimerDuration() {
+        reset()
+
+        didStop?(true)
+    }
+
     // MARK: - private
 
-    private func registerTimer() {
-        timer = Timer.scheduledTimer(timeInterval: currentDuration,
-                                     target: self,
-                                     selector: #selector(didFinishTimerDuration),
-                                     userInfo: nil,
-                                     repeats: false)
+    private func registerTimer(with startDate: Date) {
+        timer = Timer(fireAt: startDate,
+                      interval: currentDuration,
+                      target: self,
+                      selector: #selector(didFinishTimerDuration),
+                      userInfo: nil,
+                      repeats: false)
+        RunLoop.current.add(timer!, forMode: RunLoopMode.defaultRunLoopMode)
     }
 
     private func reset() {
         self.currentDuration = duration
-        self.startDate       = nil
         self.timer?.invalidate()
     }
 }
